e83a9d40cb8e11e8d0663336bae3dc8fe6752cd1
Ryosuke Takeuhi (MBPr15) <rysk.takeuchi@gmail.com>
2015-03-25 19:47:59 +0900

    mdfSummary: bug fixed

--- mdfSummary.m
+++ mdfSummary.m
@@ -1,4 +1,4 @@
-function mcs_summary = mdfSummary(mdffile, avgnframes)
+function mdf_summary = mdfSummary(mdffile, avgnframes)
 % function mcs_summary = mcsSummary(mdffile, avgnframes)
 %   mdffile   : .MDF-file name ( or path)
 %   avgnframes: The number of frames for averaging image
@@ -9,17 +9,17 @@ disp('Loading MDF-File...')
 f             = figure(1000); 
 mfile         = actxcontrol('MCSX.Data', [0, 0, 500, 500], f);
 openResult    = mfile.invoke('OpenMCSFile', mdffile);
-mcs_summary   = mcsxInfo(mfile);
+mdf_summary   = mcsxInfo(mfile);
 
 if openResult ==1
     disp([mcsfile ' succesfully loaded'])        
 end
 
 if nargin > 1
-    curCh = Get_specFrames(mfile, 1, 1, avgnframes, mcs_summary.frameheight, false);
+    curCh = mcsxReadFrames(mfile, 1, 1, avgnframes);
     curCh = curCh./max(curCh(:));
     frames(:,:,1) = mean(curCh, 3);
-    curCh = Get_specFrames(mfile, 2, 1, avgnframes, mcs_summary.frameheight, false);
+    curCh = mcsxReadFrames(mfile, 2, 1, avgnframes);
     curCh = curCh./max(curCh(:));
     frames(:,:,2) = mean(curCh, 3);
 
@@ -29,12 +29,8 @@ if nargin > 1
 		for i = 1:3
 			frames(:,:,i) = adapthisteq(frames(:,:,i));
 		end
-	end
-	
-	
-    mcs_summary.avg = frames;
-	
-	
+	end	
+    mdf_summary.avg = frames;	
 end
     
 close(1000)
